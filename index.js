3a8c9170a36f4ff2dc161a1603df0261fc13de1b
Elmedin Turkeš <elmthedev@gmail.com>
2020-03-22 10:32:34 +0100

    Additional info
    Added "additionalInfo" key to the Request.
    
    Instead of always showing the time taken user can specify if they want to see it.
    
    Also added last URL of redirects to additionalInfo called 'finalURL'

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { Curl } = require('node-libcurl')
+const {
+	Curl
+} = require('node-libcurl')
 const tough = require('tough-cookie')
 const deepmerge = require('deepmerge');
 
@@ -31,8 +33,8 @@ request = async (opts) => {
 		} else {
 			curl.setOpt('URL', opts.url)
 		}
-		
-		
+
+
 
 		// Set default request method to GET
 		if (typeof opts.strictSSL !== 'undefined' && opts.strictSSL) {
@@ -116,7 +118,7 @@ request = async (opts) => {
 						try {
 							const cookies = tough.Cookie.parse(opts.headers[header]).toString()
 							headers.push(`${header}: ${cookies}`)
-						} catch(err) {
+						} catch (err) {
 							throw new Error(`Cookie error: ${err.message}`)
 						}
 					}
@@ -164,7 +166,7 @@ request = async (opts) => {
 				if (header != 'result') {
 					headerList[header] = respHeaders[header]
 				}
-				
+
 				// Append the set cookies to their jar if they are using
 				if (header.toLowerCase() == 'set-cookie' && opts.jar) {
 					respHeaders[header].forEach(el => {
@@ -190,20 +192,28 @@ request = async (opts) => {
 			}
 
 			// Create request.js similar-style response
-			const response = {
+			let response = {
 				body: body,
 				headers: headerList,
-				statusCode: statusCode,
-				time: this.getInfo('TOTAL_TIME')
+				statusCode: statusCode
+			}
+
+			if (opts.additionalInfo) {
+				let additionalInfoObject = {
+					finalUrl: opts.url,
+					time: this.getInfo('TOTAL_TIME')
+				}
+
+				Object.assign(response, additionalInfoObject);
 			}
 
 			this.close();
 			resolve(response)
 		});
 
-		curl.on('error', function(err) {
-				curl.close.bind(curl)
-				reject(err)
+		curl.on('error', function (err) {
+			curl.close.bind(curl)
+			reject(err)
 		})
 
 		curl.perform();
@@ -222,4 +232,3 @@ request.defaults = (defaults = {}) => {
 }
 
 module.exports = request
-
